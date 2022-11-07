afb172926d2a96ff3dff04e4627110996d76f2ae
flobe99 <60112645+flobe99@users.noreply.github.com>
2022-11-07 23:04:33 +0100

    add astar algorithm

--- learner.py
+++ learner.py
@@ -3,6 +3,8 @@ import random
 import numpy as np
 from colorama import Fore, Back, Style
 import time
+import pyastar2d
+
 
 # https://www.geeksforgeeks.org/ml-monte-carlo-tree-search-mcts/
 
@@ -13,11 +15,11 @@ class World:
         self.dim_x = 12
         self.dim_y = 12
 
-        self.arr1 = np.zeros([self.dim_x, self.dim_y])
+        self.arr1 = np.zeros([self.dim_x, self.dim_y], dtype=np.float32)
         self.arr2 = np.zeros([self.dim_x, self.dim_y])
 
-        self.pos_x = 11
-        self.pos_y = 1
+        self.pos_x = 0
+        self.pos_y = 0
 
         self.target_x = 11
         self.target_y = 1
@@ -31,7 +33,6 @@ class World:
         self.arr1[1][5] = 1
         self.arr1[2][5] = 1
         self.arr1[3][5] = 1
-
         self.arr1[7][0] = 1
         self.arr1[7][1] = 1
         self.arr1[7][2] = 1
@@ -41,7 +42,6 @@ class World:
         self.arr1[7][6] = 1
         self.arr1[7][7] = 1
         self.arr1[7][8] = 1
-
         self.arr1[6][8] = 1
         self.arr1[5][8] = 1
         self.arr1[4][8] = 1
@@ -50,15 +50,28 @@ class World:
 
         # arr1[target_x][target_y] = 5
 
-    def heuristic(self, x1, y1):
+    def a_star_cost_array(self):
+        temp_arr = np.copy(self.arr1)
+        self.arr1[self.arr1 == 1] = 100
+        self.arr1[self.arr1 == 0] = 1
+        cost_arr = self.arr1
+        self.arr1 = np.copy(temp_arr)
+        return cost_arr
 
-        return abs(x1 - self.target_x) + abs(y1 - self.target_y)
+    def a_star(self):
+        cost_arr = self.a_star_cost_array()
+        start = (self.pos_x, self.pos_y)
+        goal = (self.target_x, self.target_y)
+        path = pyastar2d.astar_path(cost_arr, start, goal, allow_diagonal=True)
 
-    def A_star(self):
+        # The path is returned as a numpy array of (i, j) coordinates.
+        # print(f"Shortest path from {start} to {goal} found:")
 
-        depth = 0
-        while 1:
-            pass
+        for item in path:
+            self.arr1[item[0]][item[1]] = 8
+
+        self.arr1[start[0]][start[1]] = 3
+        self.arr1[goal[0]][goal[1]] = 3
 
     def position_move(self):
         pos_directions = []
@@ -107,12 +120,14 @@ class World:
         rnd_positions = self.position_move()
         print(rnd_positions)
 
-        rnd2 = random.randint(0, len(rnd_positions) - 1)
+        rnd2 = random.randint(0, len(rnd_positions))
         position_max = rnd_positions[rnd2]
         print("position chosen", position_max)
         print("")
         self.pos_x = position_max[0]
         self.pos_y = position_max[1]
+        print("########################")
+        print(self.pos_x, self.pos_y)
         self.arr1[self.pos_x][self.pos_y] = 8
         self.arr2[self.pos_x][self.pos_y] = reward_max
 
@@ -123,12 +138,15 @@ class World:
         return reward3
 
     def search(self):
-        rnd1 = random.random()
-        # print('rnd', rnd1)
-        if rnd1 < self.epsilon:
-            self.move_explore()
-        else:
-            self.move_predict()
+        for i in range(0, 1):
+            rnd1 = random.random()
+            rnd1 = 0.8130436593743464
+            print("rnd", rnd1)
+            if rnd1 < self.epsilon:
+                self.move_explore()
+            else:
+                self.move_predict()
+            self.print_board(self.arr1)
 
     def print_board(self, board):
         for row in board:
@@ -150,14 +168,19 @@ class World:
 w1 = World()
 
 # print(print(w1.arr1))
-w1.print_board(w1.arr1)
+# print("\nStart Grid")
+# w1.print_board(w1.arr1)
 
-for i in range(0, 100):
-    print("iteration nr: ", i)
-    time.sleep(1)
-    w1.search()
-    w1.print_board(w1.arr1)
+
+w1.search()  # search with random search
+
+# w1.a_star()  # search with AStar
+# print("\nAStar Path")
+# w1.print_board(w1.arr1)
+
 print("")
+print("Arr2")
 print(w1.arr2)
 print("")
+
 w1.print_board(w1.arr1)
